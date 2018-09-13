92f6355ea33768e5aefea9d06a33d35b78c2cfd4
mrvoh <mrvoh123@gmail.com>
2018-09-13 09:27:26 -0700

    first setup of DE algo

---
+++ assignmentfiles_2017/DifferentialEvolution.java
@@ -0,0 +1,179 @@
+import org.vu.contest.ContestSubmission;
+import org.vu.contest.ContestEvaluation;
+
+import java.util.Random;
+import java.util.Properties;
+import java.util.*;
+
+public class DifferentialEvolution implements ContestSubmission
+{
+
+    // PRE-PROGRAMMED STUFF
+
+	Random rnd_;
+	ContestEvaluation evaluation_;
+    private int evaluations_limit_;
+	
+	public DifferentialEvolution()
+	{
+		rnd_ = new Random();
+	}
+	
+	public void setSeed(long seed)
+	{
+		// Set seed of algortihms random process
+		rnd_.setSeed(seed);
+	}
+
+	public void setEvaluation(ContestEvaluation evaluation)
+	{
+		// Set evaluation problem used in the run
+		evaluation_ = evaluation;
+		
+		// Get evaluation properties
+		Properties props = evaluation.getProperties();
+        // Get evaluation limit
+        evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
+		// Property keys depend on specific evaluation
+		// E.g. double param = Double.parseDouble(props.getProperty("property_name"));
+        boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
+        boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
+        boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
+
+        String eval_type = props.getProperty("Evaluation");
+
+        System.out.println(eval_type);
+		// Do sth with property values, e.g. specify relevant settings of your algorithm
+        if(isMultimodal){
+            // Do sth
+        }else{
+            // Do sth else
+        }
+    }
+
+    // MAIN ALGORITH PARAMETERS
+    public int PHENOTYPE_DIM = 10; // each phenotype has 10 dimensions in our assignment
+    public int DIM_LOWER_BOUND = -5; // each dimension ranges from [-5, 5]
+    public int DIM_UPPER_BOUND = 5;
+    public int POP_SIZE = 100; // mu
+    public double SCALING_FACTOR = 0.5; //F
+    public double CROSSOVER_PROB = 0.5; // Cr
+    public double CROSSOVER_RATE = 0.1; // Pc
+
+    // Params for DE operators (different versions of algorithm)
+    public int NR_PERTURBATION_VECTORS = 1;
+    public String BASE_VECTOR = "random";
+    public String CROSSOVER_SCHEME = "bin";
+
+
+
+
+    // HELPER FUNCTIONS FOR MAIN
+    private double[][] init_population(int pop_size, int phenotype_dim){
+        // function to intialize a random population of size pop_size
+
+        double[][] pop = new double[pop_size][phenotype_dim];
+
+        for(int i = 0; i < pop_size;i++){
+            // init phenotype
+            double[] pheno = new double[phenotype_dim];
+
+            for(int j = 0; j < phenotype_dim; j++){
+                // initialize values randomly
+                pheno[j] = rnd_.nextDouble();
+                }
+            
+            pop[i] = pheno;
+
+        }
+
+        return pop;
+    }
+    private double[] init_fitness_scores(int pop_size){
+        // function to initialize an array of fitnesses with only zeros
+
+        double[] fitness = new double[pop_size];// java initializes an array with zeros
+        return fitness;
+    }
+
+    private double[] eval_pop(double[][] pop){
+        // function to evaluate each phenotype in a population
+
+        double[] dummy = new double[POP_SIZE];
+        return  dummy;
+    }
+
+    private double[][] get_mutant_vector(double[][] pop){
+        // function to create a new mutant population based on pop
+        // result dims should be [POP_SIZE][PHENOTYPE_DIM]
+
+        // Sample base vector based on BASE_VECTOR 
+
+        // create difference vector based on NR_PERTURBATION_VECTORS
+
+        // create final population
+        double[][] dummy = new double[POP_SIZE][PHENOTYPE_DIM];
+        return dummy;
+    }
+
+    private double[][] get_trial_vector(double[][] pop, double[][] mutants){
+        // function to create the trial vector T
+
+        // apply crossover based on CROSSOVER_SCHEME
+
+
+        // return T
+        double[][] dummy = new double[POP_SIZE][PHENOTYPE_DIM];
+        return dummy;
+    }
+
+    private double[] crossover(double[] parent1, double[] parent2){
+        // function to perform crossover between two parents, return new child
+
+        double[] dummy = new double[PHENOTYPE_DIM];
+        return dummy;
+    }
+
+    private List<Object> survival_selection(double[][] parents, double[] parents_fitness, double[][] children){
+        // function to select new surivors based on fitness
+
+        // evaluate children
+
+        // compare each parent to child and save fittest in new population
+
+
+        double[][] survivors = new double[POP_SIZE][PHENOTYPE_DIM];
+        double[] survivor_fitness = new double[POP_SIZE];
+
+
+        return Arrays.asList(survivors, survivor_fitness);
+
+    }
+
+
+
+
+
+
+
+    // MAIN FUNCTION
+    
+	public void run()
+	{
+		// Run your algorithm here
+        
+        int evals = 0;
+        // init population
+        // calculate fitness
+        while(evals<evaluations_limit_){
+            // Select parents
+            // Apply crossover / mutation operators
+            double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+            // Check fitness of unknown fuction
+            Double fitness = (double) evaluation_.evaluate(child);
+            evals++;
+            // Select survivors
+        }
+
+	}
+}
